90eb0c7e8fe51a9916211677d4c80b8a78b83d79
touros91 <andreageraci91@gmail.com>
2021-10-05 13:53:23 +0200

    Aggiunto struttura base

---
+++ src/components/Films.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <FilmCard/>
+    </div>
+</template>
+
+<script>
+import FilmCard from './FilmCard.vue'
+
+export default {
+    name: 'Films',
+    components: {
+        FilmCard
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="input-group mb-3 justify-content-end">
+                        <input type="text" placeholder="Cerca film">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button">Cerca</button>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
