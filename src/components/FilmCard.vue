7cee46a123404be45b37d3b451883952b5d1ecc7
touros91 <andreageraci91@gmail.com>
2021-10-06 15:38:46 +0200

    Aggiunto immagine di copertina films/series

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -5,6 +5,7 @@
             <h5>Titolo Film Originale: <strong>{{film.original_title}}</strong></h5>
             <h6>Lingua: {{film.original_language.toUpperCase()}} <lang-flag :iso='film.original_language' :squared="false"/></h6>
             <h6>Voto: {{film.vote_average}}</h6>
+            <img :src="`https://image.tmdb.org/t/p/w154/` + film.poster_path" :alt="film.original_title">
             <hr>
         </div>
         <div class="series" v-if="film.name != null">
@@ -12,6 +13,7 @@
             <h5>Titolo Serie TV Originale: <strong>{{film.original_name}}</strong></h5>
             <h6>Lingua: {{film.original_language.toUpperCase()}} <lang-flag :iso='film.original_language' :squared="false"/></h6>
             <h6>Voto: {{film.vote_average}}</h6>
+            <img :src="`https://image.tmdb.org/t/p/w154/` + film.poster_path" :alt="film.name">
             <hr>
         </div>
     </div>
